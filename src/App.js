c8c667d5a3d8f9c432d845cc81fb5714e79c9c93
Jalynn Nicoly <97772154+Jalynnn@users.noreply.github.com>
2024-12-31 15:40:39 -0700

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
+
 import './App.css';
+
+import React from 'react';
+
+// import { Helmet } from "react-helmet";
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <Helmet>
+        <title>Advanced Appliance Repair</title>
+        <meta name="description" content="Advanced Appliance Repair overview, contact, and expertise." />
+        <meta name="keywords" content="Advanced Appliance Repair, website, web development, React" />
+        <meta name="author" content="Advanced Appliance Repair" />
+      </Helmet> */}
+
+      <Header />
+
+      {/* <Home />
+      <About />
+      <Publications />
+      <Project />
+      <Gallery />
+      <Contact />
+      <Footer /> */}
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import '../CSS/Header.css';
+
+function Header() {
+
+    const [isNavOpen, setNavOpen] = useState(false);
+
+    const toggleNav = () => setNavOpen(!isNavOpen);
+
+    // Once an item is clicked on, it closes the nav
+    const closeNav = () => setNavOpen(false);
+
+    return (
+        <header className="header">
+            <nav>
+                <button 
+                    className="nav-toggle" 
+                    onClick={toggleNav}
+                    aria-expanded={isNavOpen}
+                    aria-controls="nav-menu"
+                >
+                    {/* If the Nav is open, it changes the hamburger to an X */}
+                    {isNavOpen ? '✕' : '☰'}
+                </button>
+                <ul id="nav-menu" className={isNavOpen ? "open" : ""}>
+                    <li><a href="#home" onClick={closeNav}>Home</a></li>
+                    <li><a href="#about" onClick={closeNav}>About</a></li>
+                    <li><a href="#publications" onClick={closeNav}>Publications</a></li>
+                    <li><a href="#projects" onClick={closeNav}>Projects</a></li>
+                    {/* <li><a href="#gallery" onClick={closeNav}>Gallery</a></li> */}
+                    <li><a href="#contact" onClick={closeNav}>Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header; 
